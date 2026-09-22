1942bc4a430a8f4c609237d265eba85769ee2c8b
KirtikaKhowal <kanishkakhowal21@gmail.com>
2026-09-22 09:48:40 +0530

    first commit

---
+++ Assignment-2/data/students.js
@@ -0,0 +1,22 @@
+let students = [
+    {
+        id: 1,
+        name: "Kirtika",
+        age: 20,
+        course: "B.Tech CSE"
+    },
+    {
+        id: 2,
+        name: "Vaishali",
+        age: 21,
+        course: "B.Tech CSE"
+    },
+    {
+        id: 3,
+        name: "Shadav SIR",
+        age: 29,
+        course: "B.Tech AI"
+    }
+];
+
+module.exports = students;
